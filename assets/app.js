f1888820cd1b81b93f9119dfb55bc8899e970fbb
neerajyernale <yneerajk2003@gmail.com>
2026-05-18 19:07:29 +0530

    work on read functionality

---
+++ assets/app.js
@@ -0,0 +1,61 @@
+const cl = console.log;
+const todoList = document.getElementById('todoList');
+
+let todoArr = [
+    {
+        id: '1',
+        todoItem: 'Learn JS'
+    },
+    {
+        id: '2',
+        todoItem: 'Learn React'
+    },
+    {
+        id: '3',
+        todoItem: 'Learn Bootstrap'
+    }
+];
+
+if (localStorage.getItem('todoArr')) {
+    todoArr = JSON.parse(localStorage.getItem('todoArr'));
+} else {
+    localStorage.setItem('todoArr', JSON.stringify(todoArr));
+}
+
+function createArr(arr) {
+
+    let result = '';
+
+    arr.forEach(obj => {
+
+        result += `
+            <li
+                id="${obj.id}"
+                class="list-group-item d-flex justify-content-between align-items-center">
+
+                <strong>${obj.todoItem}</strong>
+
+                <div>
+
+                    <i
+                        onclick="onTodoEdit(this)"
+                        class="fa-solid fa-pen-to-square fa-2x text-primary me-3"
+                        role="button">
+                    </i>
+
+                    <i
+                        onclick="onTodoRemove(this)"
+                        class="fa-solid fa-trash fa-2x text-danger"
+                        role="button">
+                    </i>
+
+                </div>
+
+            </li>
+        `;
+    });
+
+    todoList.innerHTML = result;
+}
+
+createArr(todoArr);
