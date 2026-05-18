c10308e7e23c2870a3a4188306f9a88b9570f386
biradarsatyam66-eng <biradarsatyam66@gmail.com>
2026-05-18 23:44:04 +0530

    Added create functionality

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 const cl = console.log;
 const todoList = document.getElementById('todoList');
+const todoform = document.getElementById('todoform');
+const todoitem = document.getElementById('todoitem');
 
 let todoArr = [
     {
@@ -55,3 +57,48 @@ function createArr(arr) {
 }
 
 createArr(todoArr);
+
+
+
+
+function onformSubmit(eve){
+    eve.preventDefault();
+
+    let newtodo ={
+        todoItem : todoitem.value,
+        id : Date.now().toString()
+    }
+
+    todoform.reset();
+
+    todoArr.push(newtodo);
+
+    let li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center'
+    li.id = newtodo.id;
+    li.innerHTML = `<strong>${newtodo.todoItem}</strong>
+
+                    <div>
+
+                        <i
+                            onclick="onTodoEdit(this)"
+                            class="fa-solid fa-pen-to-square fa-2x text-primary me-3"
+                            role="button">
+                        </i>
+
+                        <i
+                            onclick="onTodoRemove(this)"
+                            class="fa-solid fa-trash fa-2x text-danger"
+                            role="button">
+                        </i>
+
+                    </div>`
+
+    todoList.appendChild(li);
+}
+
+
+
+
+
+todoform.addEventListener('submit', onformSubmit);
