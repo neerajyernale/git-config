28e6ffe1fd5cd40c4b9763b06e878345950c9e7f
neerajyernale <yneerajk2003@gmail.com>
2026-05-21 12:01:22 +0530

    change in edit functionality

--- assets/app.js
+++ assets/app.js
@@ -68,7 +68,7 @@ function onTodoEdit(ele){
         return todo.id === EDIT_ID
     });
 
-    todoitem.value = EDIT_OBJ.todoitem;
+    todoitem.value = EDIT_OBJ.todoItem;
 
     addtodobtn.classList.add('d-none');
     updatebtn.classList.remove('d-none');
